11ac5411c0127a30f6ce196e13d325b6376a085b
Jordan Renaud <jtrenaud1s@semo.edu>
2023-08-08 21:47:14 -0500

    Spring Boot 3.1.2, Java 17, Lombok, JPA, H2, Postgres, Spring-Security

---
+++ src/main/java/io/subspacedev/springbootrestboilerplate/SpringBootBackendRestBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package io.subspacedev.springbootrestboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestBoilerplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/subspacedev/springbootrestboilerplate/SpringBootRestBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.subspacedev.springbootrestboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestBoilerplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
